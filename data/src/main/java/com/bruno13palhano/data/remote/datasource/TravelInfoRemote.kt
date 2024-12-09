46ab9ac363434707c58baab09b5334e7b91e6114
bruno13palhano <bruno13palhano@gmail.com>
2024-12-09 00:11:42 -0300

    Implement methos to confirm ride and get rides

--- data/src/main/java/com/bruno13palhano/data/remote/datasource/TravelInfoRemote.kt
+++ data/src/main/java/com/bruno13palhano/data/remote/datasource/TravelInfoRemote.kt
@@ -1,9 +1,18 @@
 package com.bruno13palhano.data.remote.datasource
 
-import com.bruno13palhano.data.remote.model.RequestDriver
+import com.bruno13palhano.data.remote.model.DriverRequest
 import com.bruno13palhano.data.remote.model.RideEstimateResponse
 import com.bruno13palhano.data.model.Resource
+import com.bruno13palhano.data.remote.model.ConfirmRideRequest
+import com.bruno13palhano.data.remote.model.ConfirmRideResponse
+import com.bruno13palhano.data.remote.model.RidesResponse
 
 internal interface TravelInfoRemote {
-    suspend fun searchDriver(requestDriver: RequestDriver): Resource<RideEstimateResponse>
+    suspend fun searchDriver(driverRequest: DriverRequest): Resource<RideEstimateResponse>
+
+    suspend fun confirmRide(confirmRideRequest: ConfirmRideRequest): Resource<ConfirmRideResponse>
+
+    suspend fun getCustomerRides(customerId: String, driverId: Long): Resource<RidesResponse>
+
+    suspend fun getRides(customerId: String, driverId: Long): Resource<RidesResponse>
 }
--- data/src/main/java/com/bruno13palhano/data/remote/datasource/TravelInfoRemoteImpl.kt
+++ data/src/main/java/com/bruno13palhano/data/remote/datasource/TravelInfoRemoteImpl.kt
@@ -1,32 +1,63 @@
 package com.bruno13palhano.data.remote.datasource
 
 import com.bruno13palhano.data.model.ErrorResponse
-import com.bruno13palhano.data.remote.model.RequestDriver
+import com.bruno13palhano.data.remote.model.DriverRequest
 import com.bruno13palhano.data.remote.model.RideEstimateResponse
 import com.bruno13palhano.data.remote.model.RemoteErrorResponse
 import com.bruno13palhano.data.model.Resource
 import com.bruno13palhano.data.model.asExternal
+import com.bruno13palhano.data.remote.model.ConfirmRideRequest
+import com.bruno13palhano.data.remote.model.ConfirmRideResponse
+import com.bruno13palhano.data.remote.model.RidesResponse
 import com.bruno13palhano.data.remote.service.Service
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Response
 import javax.inject.Inject
 
 internal class TravelInfoRemoteImpl @Inject constructor(
     private val service: Service
 ) : TravelInfoRemote {
-    override suspend fun searchDriver(requestDriver: RequestDriver): Resource<RideEstimateResponse> {
-        val response = service.findDriver(request = requestDriver)
+    override suspend fun searchDriver(driverRequest: DriverRequest): Resource<RideEstimateResponse> {
+        val response = service.findDriver(request = driverRequest)
 
+        return getResponse(response = response)
+    }
+
+    override suspend fun confirmRide(confirmRideRequest: ConfirmRideRequest): Resource<ConfirmRideResponse> {
+        val response = service.confirmRide(request = confirmRideRequest)
+
+        return getResponse(response = response)
+    }
+
+    override suspend fun getCustomerRides(
+        customerId: String,
+        driverId: Long
+    ): Resource<RidesResponse> {
+        val response = service.getCustomerRides(customerId = customerId, driverId = driverId)
+
+        return getResponse(response = response)
+    }
+
+    override suspend fun getRides(customerId: String, driverId: Long): Resource<RidesResponse> {
+        val response = service.getCustomerRides(customerId = customerId, driverId = driverId)
+
+        return getResponse(response = response)
+    }
+
+    private fun <T> getResponse(response: Response<T>): Resource<T> {
         val result = response.body()
 
         return try {
             if (response.isSuccessful && result != null) {
-                Resource.Success(result)
+                Resource.Success(data = result)
             } else {
-                Resource.ServerResponseError(getInvalidResponse(response.errorBody()!!.string()))
+                Resource.ServerResponseError(
+                    errorResponse = getInvalidResponse(response = response.errorBody()!!.string())
+                )
             }
         } catch (e: Exception) {
-            Resource.Error(e.message.toString())
+            Resource.Error(message = e.message.toString())
         }
     }
 
