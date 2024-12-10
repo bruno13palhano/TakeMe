5b8810f5be4c904b35d67d5d7605cfbe5953bf02
bruno13palhano <bruno13palhano@gmail.com>
2024-12-09 22:56:46 -0300

    Implement converters

--- data/src/main/java/com/bruno13palhano/data/model/DriverInfo.kt
+++ data/src/main/java/com/bruno13palhano/data/model/DriverInfo.kt
@@ -1,9 +1,25 @@
 package com.bruno13palhano.data.model
 
+import com.bruno13palhano.data.local.model.DriverInfoEntity
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class DriverInfo(
     val id: Long,
     val name: String
+) {
+    companion object {
+        val empty = DriverInfo(id = 0, name = "")
+    }
+}
+
+
+internal fun DriverInfo.asInternal() = DriverInfoEntity(
+    id = id,
+    name = name
+)
+
+internal fun DriverInfoEntity.asExternal() = DriverInfo(
+    id = id,
+    name = name
 )
