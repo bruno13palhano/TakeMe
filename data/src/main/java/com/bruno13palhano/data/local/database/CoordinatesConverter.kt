57cca80b55e97197786a838fa8db591ff3605914
bruno13palhano <bruno13palhano@gmail.com>
2024-12-10 23:39:24 -0300

    Refactor

--- data/src/main/java/com/bruno13palhano/data/local/database/CoordinatesConverter.kt
+++ data/src/main/java/com/bruno13palhano/data/local/database/CoordinatesConverter.kt
@@ -2,16 +2,17 @@ package com.bruno13palhano.data.local.database
 
 import androidx.room.TypeConverter
 import com.bruno13palhano.data.model.Coordinates
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 internal class CoordinatesConverter {
     @TypeConverter
     fun fromCoordinates(coordinates: Coordinates): String {
-        return "${coordinates.latitude},${coordinates.longitude}"
+        return Json.encodeToString(coordinates)
     }
 
     @TypeConverter
     fun toCoordinates(value: String): Coordinates {
-        val parts = value.split(",")
-        return Coordinates(parts[0].toDouble(), parts[1].toDouble())
+        return Json.decodeFromString(value)
     }
 }
