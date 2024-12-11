b8c622cbb71741565aeff7df2849f6bb7936cf54
bruno13palhano <bruno13palhano@gmail.com>
2024-12-11 12:11:06 -0300

    Remove unused import

--- data/src/main/java/com/bruno13palhano/data/remote/model/helper/StepRemote.kt
+++ data/src/main/java/com/bruno13palhano/data/remote/model/helper/StepRemote.kt
@@ -2,7 +2,6 @@ package com.bruno13palhano.data.remote.model.helper
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import kotlinx.serialization.Serializable
 
 @JsonClass(generateAdapter = true)
 internal data class StepRemote(
