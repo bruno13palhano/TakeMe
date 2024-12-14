333486d1a4d2dc9f876d57d0fa7d468861fdcaea
bruno13palhano <bruno13palhano@gmail.com>
2024-12-14 10:53:44 -0300

    Add FakeRideEstimatRepository

---
+++ app/src/test/java/com/bruno13palhano/takeme/repository/FakeRideEstimateRepository.kt
@@ -0,0 +1,43 @@
+package com.bruno13palhano.takeme.repository
+
+import com.bruno13palhano.data.model.ErrorResponse
+import com.bruno13palhano.data.model.InternalError
+import com.bruno13palhano.data.model.Resource
+import com.bruno13palhano.data.model.RideEstimate
+import com.bruno13palhano.data.repository.RideEstimateRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+internal class FakeRideEstimateRepository : RideEstimateRepository {
+    private val rides = mutableListOf<RideEstimate>()
+
+    override suspend fun insertRideEstimate(rideEstimate: RideEstimate) {
+        rides.add(rideEstimate)
+    }
+
+    override suspend fun searchDriver(
+        customerId: String?,
+        origin: String?,
+        destination: String?
+    ): Resource<RideEstimate> {
+        val randomIndex = (rides.indices).random()
+
+        return when (customerId) {
+            "ServerErrorResponse" -> {
+                Resource.ServerResponseError(
+                    errorResponse = ErrorResponse("401", "Unauthorized")
+                )
+            }
+
+            "InternalError" -> {
+                Resource.Error(internalError = InternalError.UNKNOWN_ERROR)
+            }
+
+            else -> Resource.Success(rides[randomIndex])
+        }
+    }
+
+    override fun getLastRideEstimate(): Flow<RideEstimate?> {
+        return flowOf(rides.lastOrNull())
+    }
+}
