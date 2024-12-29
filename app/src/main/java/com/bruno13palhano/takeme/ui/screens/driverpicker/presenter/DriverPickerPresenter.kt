59238090d3a931422b7f8b28e746b52a18cba36b
bruno13palhano <bruno13palhano@gmail.com>
2024-12-29 15:46:20 -0300

    Refactor presenters

--- app/src/main/java/com/bruno13palhano/takeme/ui/screens/driverpicker/presenter/DriverPickerPresenter.kt
+++ app/src/main/java/com/bruno13palhano/takeme/ui/screens/driverpicker/presenter/DriverPickerPresenter.kt
@@ -1,29 +1,24 @@
 package com.bruno13palhano.takeme.ui.screens.driverpicker.presenter
 
 import com.bruno13palhano.data.di.ConfirmRideRep
-import com.bruno13palhano.data.di.IOScope
 import com.bruno13palhano.data.di.RideEstimateRep
 import com.bruno13palhano.data.repository.ConfirmRideRepository
 import com.bruno13palhano.data.repository.RideEstimateRepository
 import com.bruno13palhano.takeme.ui.screens.di.DefaultDriverPickerReducer
 import com.bruno13palhano.takeme.ui.screens.di.DefaultDriverPickerState
 import com.bruno13palhano.takeme.ui.shared.base.BasePresenter
-import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 
 internal class DriverPickerPresenter @Inject constructor(
     @RideEstimateRep private val rideEstimateRepository: RideEstimateRepository,
     @ConfirmRideRep private val confirmRideRepository: ConfirmRideRepository,
     @DefaultDriverPickerState initialState: DriverPickerState,
-    @DefaultDriverPickerReducer defaultDriverPickerReducer: DriverPickerReducer,
-    @IOScope private val ioScope: CoroutineScope
+    @DefaultDriverPickerReducer defaultDriverPickerReducer: DriverPickerReducer
 ) : BasePresenter<DriverPickerAction, DriverPickerEvent, DriverPickerState, DriverPickerSideEffect>(
     initialState = initialState,
     actionProcessor = DriverPickerActionProcessor(
         rideEstimateRepository = rideEstimateRepository,
-        confirmRideRepository = confirmRideRepository,
-        scope = ioScope
+        confirmRideRepository = confirmRideRepository
     ),
-    reducer = defaultDriverPickerReducer,
-    scope = ioScope
+    reducer = defaultDriverPickerReducer
 )
--- app/src/main/java/com/bruno13palhano/takeme/ui/screens/travelhistory/presenter/TravelHistoryPresenter.kt
+++ app/src/main/java/com/bruno13palhano/takeme/ui/screens/travelhistory/presenter/TravelHistoryPresenter.kt
@@ -1,29 +1,24 @@
 package com.bruno13palhano.takeme.ui.screens.travelhistory.presenter
 
 import com.bruno13palhano.data.di.DriverInfoRep
-import com.bruno13palhano.data.di.IOScope
 import com.bruno13palhano.data.di.RidesRep
 import com.bruno13palhano.data.repository.DriverInfoRepository
 import com.bruno13palhano.data.repository.RidesRepository
 import com.bruno13palhano.takeme.ui.screens.di.DefaultTravelHistoryReducer
 import com.bruno13palhano.takeme.ui.screens.di.DefaultTravelHistoryState
 import com.bruno13palhano.takeme.ui.shared.base.BasePresenter
-import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 
 internal class TravelHistoryPresenter @Inject constructor(
     @DriverInfoRep private val driverInfoRepository: DriverInfoRepository,
     @RidesRep private val ridesRepository: RidesRepository,
     @DefaultTravelHistoryState private val initialTravelHistoryState: TravelHistoryState,
-    @DefaultTravelHistoryReducer private val  travelHistoryReducer: TravelHistoryReducer,
-    @IOScope private val ioScope: CoroutineScope
+    @DefaultTravelHistoryReducer private val  travelHistoryReducer: TravelHistoryReducer
 ) : BasePresenter<TravelHistoryAction, TravelHistoryEvent, TravelHistoryState, TravelHistorySideEffect>(
     initialState = initialTravelHistoryState,
     actionProcessor = TravelHistoryActionProcessor(
         driverInfoRepository = driverInfoRepository,
-        ridesRepository = ridesRepository,
-        scope = ioScope
+        ridesRepository = ridesRepository
     ),
-    reducer = travelHistoryReducer,
-    scope = ioScope
+    reducer = travelHistoryReducer
 )
