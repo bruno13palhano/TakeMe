ae68d228d7825138a8642604bdfbf0872ed0e8fd
bruno13palhano <bruno13palhano@gmail.com>
2024-12-30 15:48:17 -0300

    Add benchmark

--- data/src/main/java/com/bruno13palhano/data/local/datasource/RideEstimateLocal.kt
+++
@@ -1,7 +0,0 @@
-package com.bruno13palhano.data.local.datasource
-
-internal interface RideEstimateLocal<T> {
-    suspend fun insert(rideEstimate: T)
-
-    suspend fun getLastRideEstimate(): T?
-}
